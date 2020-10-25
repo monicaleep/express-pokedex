e946549981593fbd489e2e859824028e82cf258f
monicaleep <monica.parrillo@gmail.com>
2020-10-25 17:51:51 -0400

    get and post working for favorites

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
